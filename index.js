c6d5c05f570897fa4e07fb4374a76021a758f1b9
이광훈 <leegh963@naver.com>
2023-09-27 22:59:43 +0900

    JWT 토큰 시험 템플릿

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import router from './router.js';
+
+const app = express()
+const PORT = 3002;
+
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/' , router)
+
+app.listen(PORT , () =>{
+    console.log(`Server is running on port ${PORT}`)
+})  
